5e94b65134af4c83db80891fa19451be986b4c57
ikennaUkwueze <ikennavictorofficial@gmail.com>
2021-01-27 06:07:40 +0100

    Redux thunk for dishes

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
